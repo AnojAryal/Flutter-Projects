462f65d24d338da19e798f0dd1106f979c39b2b9
anojaryal <anoj1810@gmail.com>
2023-09-26 21:59:13 +0545

    configure widgets and objects

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!',style:TextStyle(
+              color: Colors.white,
+              fontSize: 28.0,
+            ),),
+          ),
+        ),
+      ),
+    ),
+  );
 }
