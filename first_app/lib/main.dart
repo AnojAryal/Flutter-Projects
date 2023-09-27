8bd12eb0357b269b991938682574568f81e2af8f
anojaryal <anoj1810@gmail.com>
2023-09-27 23:00:08 +0545

    splitting codes across files

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,28 +12,3 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context){
-    return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!',style:TextStyle(
-              color: Colors.white,
-              fontSize: 28.0,
-            ),),
-          ),
-        );
-  }
-}
