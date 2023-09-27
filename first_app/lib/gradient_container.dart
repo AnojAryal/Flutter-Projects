150a56bec3b033c80af9d71a014aaf33b38ec1ed
anojaryal <anoj1810@gmail.com>
2023-09-27 23:11:50 +0545

    creating a custom widget

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
@@ -17,10 +18,7 @@ class GradientContainer extends StatelessWidget {
             ),
           ),
           child: const Center(
-            child: Text('Hello World!',style:TextStyle(
-              color: Colors.white,
-              fontSize: 28.0,
-            ),),
+            child: StyledText()
           ),
         );
   }
