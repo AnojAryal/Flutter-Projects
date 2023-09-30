f95aeecb3218f6992290916d2b273e467ab160ab
anojaryal <anoj1810@gmail.com>
2023-09-30 18:29:19 +0545

    styling buttons and working with padding

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -8,6 +8,8 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colors;
 
+  void rollDice() {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,11 +21,19 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Image.asset(
           'assets/images/dice-2.png',
           width: 250,
         ),
-      ),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                padding:const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle:const TextStyle(fontSize: 28)),
+            child: const Text('Roll Dice'))
+      ])),
     );
   }
 }
