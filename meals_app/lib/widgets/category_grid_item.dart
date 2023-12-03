b5ea8955b66a5b03c9bb26365d5003fa426b61df
anojaryal <anoj1810@gmail.com>
2023-12-03 16:00:26 +0545

    setting up screens and widgets

---
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget{
+  const CategoryGridItem({super.key , required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+   return ;
+  }
+}
