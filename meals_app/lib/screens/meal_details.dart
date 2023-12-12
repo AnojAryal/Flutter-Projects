1268843daa021fed0c97dd50ca45ea315c78cec4
anojaryal <anoj1810@gmail.com>
2023-12-12 20:58:24 +0545

    adding ingredients to meals_detail page

--- meals_app/lib/screens/meal_details.dart
+++ meals_app/lib/screens/meal_details.dart
@@ -12,16 +12,30 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-        meal.title,
-      )),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
-      ),
-    );
+        appBar: AppBar(
+            title: Text(
+          meal.title,
+        )),
+        body: Column(children: [
+          Image.network(
+            meal.imageUrl,
+            width: double.infinity,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 12),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 18,),
+          for (final ingredient in meal.ingredients)
+            Text(ingredient ,style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),)
+        ]));
   }
 }
