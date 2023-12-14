5ca02709b4f753e972232d5d5ac89f95b0a450c4
anojaryal <anoj1810@gmail.com>
2023-12-14 12:19:29 +0545

    selecting and highliting tabs

--- meals_app/lib/screens/tabs.dart
+++ meals_app/lib/screens/tabs.dart
@@ -38,6 +38,7 @@ class _TabsScreenState extends State<TabsScreen> {
     body: activePage,
     bottomNavigationBar: BottomNavigationBar(
       onTap: _selectPage,
+      currentIndex: _selectedPageIndex,
       items:const  [
         BottomNavigationBarItem(icon: Icon(Icons.set_meal) , label:'Categories', ),
         BottomNavigationBarItem(icon: Icon(Icons.star) , label:'Favourates',),
