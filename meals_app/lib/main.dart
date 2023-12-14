a2a25aeec9b74eb90856aaa398317f82d51b9b78
anojaryal <anoj1810@gmail.com>
2023-12-14 12:16:47 +0545

    adding tabsscreen with buttomnavigationbar

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/tabs.dart';
 
 
 final theme = ThemeData(
@@ -24,7 +25,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen()
+      home: const TabsScreen()
     );
   }
 }
