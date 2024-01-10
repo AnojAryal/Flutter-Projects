f9b1550bcb28afc92b69cda3f65fd9a9f33fa1fa
anojaryal <anoj1810@gmail.com>
2024-01-10 12:28:17 +0545

    using slide transition

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -80,11 +80,15 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             )
         ],
       ),
-      builder: (context, child) => Padding(
-          padding: EdgeInsets.only(
-            top:100 - _animationController.value * 100,
+      builder: (context, child) => SlideTransition(
+        position: _animationController.drive(
+          Tween(
+            begin:const Offset(0, 0.3) ,
+            end: const Offset(0, 0),
           ),
-          child: child),
+        ),
+        child: child,
+      ),
     );
   }
 }
